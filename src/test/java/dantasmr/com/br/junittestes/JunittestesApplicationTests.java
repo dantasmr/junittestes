f97a52f783fd53dfb2ffc4ffd0d2a5c38b0f9901
Marcelo <dantasmr@yahoo.com.br>
2020-02-15 00:22:44 -0300

    first commit

---
+++ src/test/java/dantasmr/com/br/junittestes/JunittestesApplicationTests.java
@@ -0,0 +1,13 @@
+package dantasmr.com.br.junittestes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunittestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
